abdad31ebd07e3ef1ea1381ed4e7d1ae6d1885c7
Se7en Oussama <seven.tdr7@gmail.com>
2020-08-22 12:45:55 +0200

    initialize def for event and user

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,15 @@
+require 'time'
+
 class Event
+
+    attr_accessor :start_date, :duration, :title, :attendees
+
+    def initialize(start_date, duration, title, attendees)
+        @start_date = Time.parse(start_date.to_s)
+        @duration = duration.to_i
+        @title = title.to_s
+        @attendees = []
+        @attendees << attendees.to_s
+    end
     
 end
