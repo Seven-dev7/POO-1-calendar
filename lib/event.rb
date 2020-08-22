38f03a41aeb81d3e8312fe7f696cd54f4fabe2d8
Se7en Oussama <seven.tdr7@gmail.com>
2020-08-22 13:04:02 +0200

    add def postpone24h and end_date

--- lib/event.rb
+++ lib/event.rb
@@ -12,4 +12,11 @@ class Event
         @attendees << attendees.to_s
     end
     
+    def postpone_24h
+        @start_date = Time.parse(start_date.to_s) + 3600
+    end
+
+    def end_date
+        end_date = @start_date + @duration*60
+    end
 end
